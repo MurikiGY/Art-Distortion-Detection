66935bd15941d177bd34a04e2ea7870ef276e1a8
Arthur Martinelli <ama18@inf.ufpr.br>
2023-06-25 21:15:16 -0300

    adicionado rascunho do dataset

--- ai/dataset.py
+++ ai/dataset.py
@@ -1,35 +1,30 @@
 import os
-from collections import deque
-
-working_dir = './'
-init_dir = working_dir+"data"
-
-fila = deque([init_dir])
-items= [0, 0]
-while len(fila) > 0:
-    item = fila.popleft()
-    if os.path.isdir(item):
-        for i in os.listdir(item):
-            if item != ".":
-                fila.append(item+"/"+i)
-
-    elif os.path.isfile(item):
-        if "original" in item:
-            items[0] += 1
-        elif "modified" in item:
-            items[1] += 1
-        else:
-            print("ERROR: ", item, " neither original nor modified (???)")
-    else:
-        print("ERROR: analyzing something that is neither a file nor a dir (", item, ")")
-print(items)
 
 class ImageDataset(Dataset):
 
     def __init__(self, annotations, img_dir, transform=None, target_transform=None):
-        
+        self.items = [len(os.listdir(img_dir+"original")), len(os.listdir(img_dir+"modified"))]
+        self.working_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_trasform
+
 
     def __len__(self):
-        return items[0]+items[1]
+        return self.items[0]+self.items[1]
 
     def __getitem__(self, idx):
+        if idx > self.items[0]:
+            image = read_image(self.working_dir+"original/ori_"+str(idx+1)+".jpg")
+            label = 0
+        elif idx < self.items[0]+self.items[1]:
+            image = read_image(self.working_dir+"modified/mod_"+str(idx+1-self.items[0])+".jpg")
+            label = 1
+        else:
+            print("ERROR: Index higher than limit")
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image, label
